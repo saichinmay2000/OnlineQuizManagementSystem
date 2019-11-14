1ec6f9540e6bf14d78489899adebc5cfd12681b3
saichinmay2000 <47316906+saichinmay2000@users.noreply.github.com>
2019-11-14 09:22:34 +0530

    Create MainProject.java

---
+++ MainProject.java
@@ -0,0 +1,191 @@
+import java.awt.event.*;  
+import javax.swing.*;  
+  
+class MainProject extends JFrame implements ActionListener  
+{  
+    JLabel l;
+    JRadioButton jb[]=new JRadioButton[5];  
+    JButton b1,b2,b3;
+    ButtonGroup bg;  
+    int count=0,current=0,x=1,y=1,now=0;  
+    int m[]=new int[10];      
+    MainProject(String s)  
+    {  
+        super(s);  
+        l=new JLabel();  
+        add(l);  
+        bg=new ButtonGroup();  
+        for(int i=0;i<5;i++)  
+        {  
+            jb[i]=new JRadioButton();     
+            add(jb[i]);  
+            bg.add(jb[i]);  
+        }  
+        b1=new JButton("Next");  
+        b2=new JButton("Exit");
+        b3=new JButton("Previous");
+        b1.addActionListener(this);
+        b2.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                if(e.getActionCommand().equals("Exit"))
+                {
+                    System.exit(0);
+                }
+            }
+        });
+        b3.addActionListener(this);
+        add(b1);add(b2);add(b3);
+        set();  
+        l.setBounds(60,20,450,50);  
+        jb[0].setBounds(50,80,100,20);  
+        jb[1].setBounds(50,110,100,20);  
+        jb[2].setBounds(50,140,100,20);  
+        jb[3].setBounds(50,170,100,20);  
+        b1.setBounds(100,240,100,30);  
+        b2.setBounds(340,240,100,30);
+        b3.setBounds(220,240,100,30);  
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        setLayout(null);  
+        setLocation(250,100);  
+        setVisible(true);  
+        setSize(500,400);
+        setAlwaysOnTop(true);
+        setResizable(false);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        setUndecorated(true);
+        
+    }  
+    public void actionPerformed(ActionEvent e)  
+    {  
+        if(e.getSource()==b1)  
+        {  
+            if(check())  
+                count=count+1;  
+            current++;  
+            set();    
+            if(current==9)  
+            {  
+                b1.setEnabled(false);  
+                b2.setText("Result");  
+            }
+            if(current>0)
+            {
+                b3.setEnabled(true);
+            }  
+        }
+        if(e.getSource() == b3)
+        {
+            if(check())
+                count = count-1;
+                current--;
+                set();
+                if(current==0)
+                {
+                    b3.setEnabled(false);
+                    b3.setText("Previous");
+                }    
+                if(current>9)  
+                {  
+                    b1.setEnabled(true);  
+                    b2.setText("Result");  
+                }
+        }
+
+        if(e.getActionCommand().equals("Result"))  
+        {  
+            if(check())  
+                count=count+1;  
+            current++;  
+            JOptionPane.showMessageDialog(this,"correct ans="+count);  
+            System.exit(0);  
+        }  
+    }
+    void set()  
+    {  
+        jb[4].setSelected(true);  
+        if(current==0)  
+        {  
+            l.setText("<html>Question 1: <br/> Which one among these is not a primitive datatype ? </html>");  
+            jb[0].setText("int");jb[1].setText("Float");jb[2].setText("boolean");jb[3].setText("char");   
+        }  
+        if(current==1)  
+        {  
+            l.setText("<html>Question 2: <br/> Which class is available to all the class automatically? </html>");  
+            jb[0].setText("Swing");jb[1].setText("Applet");jb[2].setText("Object");jb[3].setText("ActionEvent");  
+        }  
+        if(current==2)  
+        {  
+            l.setText("<html>Question 3: <br/> Which package is directly available to our class without importing it? </html>");  
+            jb[0].setText("swing");jb[1].setText("applet");jb[2].setText("net");jb[3].setText("lang");  
+        }  
+        if(current==3)  
+        {  
+            l.setText("<html>Question 4: <br/> String class is defined in which package? </html>");  
+            jb[0].setText("lang");jb[1].setText("Swing");jb[2].setText("Applet");jb[3].setText("awt");  
+        }  
+        if(current==4)  
+        {  
+            l.setText("<html>Question 5: <br/> String class defined in which package? </html");  
+            jb[0].setText("Utek");jb[1].setText("Aptech");jb[2].setText("SSS IT");jb[3].setText("jtek");  
+        }  
+        if(current==5)  
+        {  
+            l.setText("<html>Question 6: <br/> Which among these is not a keyword? </html>");  
+            jb[0].setText("class");jb[1].setText("int");jb[2].setText("get");jb[3].setText("if");  
+        }  
+        if(current==6)  
+        {  
+            l.setText("<html>Question 7: <br/> Which one among these is not a class? </html>");  
+            jb[0].setText("Swing");jb[1].setText("Actionperformed");jb[2].setText("ActionEvent");  
+                        jb[3].setText("Button");  
+        }  
+        if(current==7)  
+        {  
+            l.setText("<html>Question 8:<br/> Which one among these is not a function of Object class? </html>");  
+            jb[0].setText("toString");jb[1].setText("finalize");jb[2].setText("equals");  
+                        jb[3].setText("getDocumentBase");         
+        }  
+        if(current==8)  
+        {  
+            l.setText("<html>Question 9: <br/> Which function is not present in Applet class? </html>");  
+            jb[0].setText("init");jb[1].setText("main");jb[2].setText("start");jb[3].setText("destroy");  
+        }  
+        if(current==9)  
+        {  
+            l.setText("<html>Question 10: <br/> Which one among these is not a valid component? </html>");  
+            jb[0].setText("JButton");jb[1].setText("JList");jb[2].setText("JButtonGroup");  
+                        jb[3].setText("JTextArea");  
+        }  
+        l.setBounds(60,20,450,50);  
+        for(int i=0,j=0;i<=90;i+=30,j++)  
+            jb[j].setBounds(50,80+i,200,20);  
+    }  
+    boolean check()  
+    {  
+        if(current==0)  
+            return(jb[1].isSelected());  
+        if(current==1)  
+            return(jb[2].isSelected());  
+        if(current==2)  
+            return(jb[3].isSelected());  
+        if(current==3)  
+            return(jb[0].isSelected());  
+        if(current==4)  
+            return(jb[2].isSelected());  
+        if(current==5)  
+            return(jb[2].isSelected());  
+        if(current==6)  
+            return(jb[1].isSelected());  
+        if(current==7)  
+            return(jb[3].isSelected());  
+        if(current==8)  
+            return(jb[1].isSelected());  
+        if(current==9)  
+            return(jb[2].isSelected());  
+        return false;  
+    }  
+    public static void main(String s[])  
+    {  
+        new MainProject("Online Quiz Management System");  
+    }  
+}
